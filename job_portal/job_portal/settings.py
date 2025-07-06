8d2d142a68da86810f97420f67ad1de812db0b7a
Avinash <avinashnarwariya5@gmail.com>
2025-07-06 23:12:33 +0530

    fixed a secret leak

--- job_portal/job_portal/settings.py
+++ job_portal/job_portal/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-v0_pozykdd=d2!%kc0g-&(%oh7lz7b6pduk_%l)b=1!&amq23f"
+SECRET_KEY = os.getenv("DJANGO_SECRET")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
